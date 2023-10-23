00b09b5e5a998e01efe416128a3ba5de200b8b28
faisalkimz <mbabalifaisal2@gmail.com>
2023-10-23 18:21:35 +0300

    first commit

---
+++ app/src/main/java/com/example/foodieapp/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.foodieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class IntroActivity extends AppCompatActivity {
+private TextView startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        startButton = findViewById(R.id.startbutton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+
+}
