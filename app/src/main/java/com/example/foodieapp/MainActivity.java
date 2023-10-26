47843fc51652bf5c1a8319c2a3f099366329183d
faisalkimz <mbabalifaisal2@gmail.com>
2023-10-26 15:21:55 +0300

    more

--- app/src/main/java/com/example/foodieapp/MainActivity.java
+++ app/src/main/java/com/example/foodieapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.foodieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavigationview);
+        bottomNavigationView.setBackground(null);
+        Menu menu = bottomNavigationView.getMenu();
+        menu.getItem(2).setEnabled(false);
     }
 }
