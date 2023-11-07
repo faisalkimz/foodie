7beeee6c46314d2ca11774ba1f289946c0046dcc
faisalkimz <mbabalifaisal2@gmail.com>
2023-11-07 12:53:56 +0300

    more

---
+++ app/src/main/java/com/example/foodieapp/Domain/categoryDomain.java
@@ -0,0 +1,27 @@
+package com.example.foodieapp.Domain;
+
+public class categoryDomain {
+    private String name;
+    private String pic;
+
+    public categoryDomain(String name, String pic) {
+        this.name = name;
+        this.pic = pic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+}
--- app/src/main/java/com/example/foodieapp/MainActivity.java
+++ app/src/main/java/com/example/foodieapp/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.foodieapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.foodieapp.Domain.categoryDomain;
 
 import android.os.Bundle;
 import android.view.Menu;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Locale;
 
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnavigationview);
-        bottomNavigationView.setBackground(null);
-        Menu menu = bottomNavigationView.getMenu();
-        menu.getItem(2).setEnabled(false);
+        recyclerViewCategory();
+
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        recyclerViewCategoryList=findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+        ArrayList<categoryDomain> category=new ArrayList<>();
+        category.add(new categoryDomain("pizza", "cat_1"));
+        category.add(new categoryDomain("Burger", "cat_2"));
+        category.add(new categoryDomain("Hot Dog", "cat_3"));
+        category.add(new categoryDomain("soda", "cat_4"));
+        category.add(new categoryDomain("Doughnuts", "cat_5"));
+
+
     }
 }
